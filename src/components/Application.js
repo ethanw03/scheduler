bf05af38189b0c13ea7366771316eb620cf3c694
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-23 17:10:27 -0800

    finished displaying appointments using mockdata. adjusted show, and index as well

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React,{ useState }  from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -20,9 +21,51 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
+
   const [day, setDay] = useState("Monday");
+  
   return (
+    
     <main className="layout">
       <section className="sidebar">
       <img
@@ -32,20 +75,23 @@ export default function Application(props) {
       />
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
+      
       <DayList
-  days={days}
-  value={day}
-  onChange={setDay}
-/>
+        days={days}
+        value={day}
+        onChange={setDay}
+      />
       </nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
+    <img
+      className="sidebar__lhl sidebar--centered"
+      src="images/lhl.png"
+      alt="Lighthouse Labs"
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {Object.values(appointments).map(appointment =>{return <Appointment key={appointment.id} name={appointment.name} {...appointment} />
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,11 +4,13 @@ import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
 
+
+
 export default function Appointment(props){
   return (
     <article className="appointment" >
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/>: <Empty />}
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/>: <Empty />}
 
     </article>
   )
