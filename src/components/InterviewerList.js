f0dfc52cbfcdb2e8b74c7ba5b1f4c859f1bf2329
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-28 18:46:01 -0800

    Type checking with prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import 'components/InterviewerList.scss'
-import InterviewerListItem from 'components/InterviewerListItem.js'
+import 'components/InterviewerList.scss';
+import InterviewerListItem from 'components/InterviewerListItem.js';
+import PropTypes from 'prop-types';
 
+function InterviewerList(props) {
+	const { interviewers } = props;
 
-export default function InterviewerList(props){
-  const {interviewers} = props;
+	const interviewersListData = interviewers.map((interviewer) => {
+		return (
+			<InterviewerListItem
+				key={interviewer.id}
+				name={interviewer.name}
+				avatar={interviewer.avatar}
+				selected={props.interviewer === interviewer.id}
+				setInterviewer={(event) => {
+					props.onChange(interviewer.id);
+				}}
+			/>
+		);
+	});
 
-  const interviewersListData = interviewers.map(interviewer =>{
-    return <InterviewerListItem 
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={props.interviewer === interviewer.id}
-      setInterviewer={event => {props.onChange(interviewer.id)}}    
-    />
-  
-  })
-
-  return(
-    <section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list" >{interviewersListData}</ul>
-</section>
-  )
+	return (
+		<section className='interviewers'>
+			<h4 className='interviewers__header text--light'>Interviewer</h4>
+			<ul className='interviewers__list'>{interviewersListData}</ul>
+		</section>
+	);
 }
+
+InterviewerList.propTypes = {
+	interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
