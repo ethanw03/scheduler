c626a19902ad07ebba263934bd9d8474b65503b5
Ethan Wong <ethanwong98@yahoo.ca>
2023-02-28 18:19:43 -0800

    app now updates spots remaining

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import DayList from './DayList';
 import 'components/Application.scss';
 import Appointment from './Appointment';
-import axios from 'axios';
 import {
 	getAppointmentsForDay,
 	getInterview,
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -32,6 +32,17 @@ export default function useApplicationData() {
 			}));
 		});
 	}, []);
+	//finds specific day
+	function findDay(day) {
+		const daysOfWeek = {
+			Monday: 0,
+			Tuesday: 1,
+			Wednesday: 2,
+			Thursday: 3,
+			Friday: 4,
+		};
+		return daysOfWeek[day];
+	}
 
 	//function that  books interview
 	function bookInterview(id, interview) {
@@ -44,6 +55,28 @@ export default function useApplicationData() {
 			[id]: appointment,
 		};
 
+		const dayOfWeek = findDay(state.day);
+
+		let day = {
+			...state.days[dayOfWeek],
+			spots: state.days[dayOfWeek],
+		};
+
+		if (!state.appointments[id].interview) {
+			day = {
+				...state.days[dayOfWeek],
+				spots: state.days[dayOfWeek].spots - 1,
+			};
+		} else {
+			day = {
+				...state.days[dayOfWeek],
+				spots: state.days[dayOfWeek].spots,
+			};
+		}
+
+		let days = state.days;
+		days[dayOfWeek] = day;
+
 		const url = `http://localhost:8001/api/appointments/${id}`;
 
 		//save data to API endpoint
@@ -53,7 +86,7 @@ export default function useApplicationData() {
 			data: appointment,
 		};
 		return axios(req).then((response) => {
-			setState({ ...state, appointments });
+			setState({ ...state, appointments, days });
 		});
 	}
 
@@ -68,6 +101,16 @@ export default function useApplicationData() {
 			[id]: appointment,
 		};
 
+		const dayOfWeek = findDay(state.day);
+
+		const day = {
+			...state.days[dayOfWeek],
+			spots: state.days[dayOfWeek].spots + 1,
+		};
+
+		let days = state.days;
+		days[dayOfWeek] = day;
+
 		const url = `http://localhost:8001/api/appointments/${id}`;
 
 		//delete data from api endpoint
@@ -78,7 +121,7 @@ export default function useApplicationData() {
 		};
 		return axios(req).then((response) => {
 			console.log('response from delete axios', response);
-			setState({ ...state, appointments });
+			setState({ ...state, appointments, days });
 		});
 	}
 
