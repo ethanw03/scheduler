ba6fda6bb9ba5b2842800f9b5790c49737e20b25
Ethan Wong <ethanwong98@yahoo.ca>
2023-03-01 22:25:30 -0800

    debugging tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,6 +10,7 @@ import {
 	getAllByTestId,
 	getByAltText,
 	getByPlaceholderText,
+	queryByText,
 } from '@testing-library/react';
 
 import Application from 'components/Application';
@@ -28,7 +29,7 @@ describe('Application', () => {
 	});
 
 	it('loads data, books an interview and reduces the spots remaining for Monday by 1', async () => {
-		const { container } = render(<Application />);
+		const { container, debug } = render(<Application />);
 
 		await waitForElement(() => getByText(container, 'Archie Cohen'));
 
@@ -40,10 +41,18 @@ describe('Application', () => {
 		fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
 			target: { value: 'Lydia Miller-Jones' },
 		});
-		fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
 
+		fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
 		fireEvent.click(getByText(appointment, 'Save'));
 
-		console.log(prettyDOM(appointment));
+		expect(getByText(appointment, 'Saving')).toBeInTheDocument();
+
+		await waitForElement(() => getByText(appointment, 'Lydia Miller-Jones'));
+
+		const day = getAllByTestId(container, 'day').find((day) =>
+			queryByText(day, 'Monday')
+		);
+
+		expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
 	});
 });
