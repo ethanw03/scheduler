c5240deb5606495320c0ef4c8e166edf7a5bc12e
Ethan Wong <ethanwong98@yahoo.ca>
2023-03-01 17:32:14 -0800

    added form tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from 'components/InterviewerList';
 export default function Form(props) {
 	const [interviewer, setInterviewer] = useState(props.interviewer || null);
 	const [name, setName] = useState(props.name || '');
+	const [error, setError] = useState('');
 
 	const reset = function () {
 		setName('');
@@ -14,6 +15,15 @@ export default function Form(props) {
 		reset();
 		props.onCancel();
 	};
+
+	function validate() {
+		if (name === '') {
+			setError('Student name cannot be blank');
+			return;
+		}
+
+		props.onSave(name, interviewer);
+	}
 
 	return (
 		<main className='appointment__card appointment__card--create'>
@@ -26,7 +36,9 @@ export default function Form(props) {
 						placeholder='Enter Student Name'
 						value={name}
 						onChange={(event) => setName(event.target.value)}
+						data-testid='student-name-input'
 					/>
+					<section className='appointment__validation'>{error}</section>
 				</form>
 				<InterviewerList
 					interviewers={props.interviewers}
@@ -39,7 +51,7 @@ export default function Form(props) {
 					<Button danger onClick={cancel}>
 						Cancel
 					</Button>
-					<Button confirm onClick={() => props.onSave(name, interviewer)}>
+					<Button confirm onClick={(event) => validate()}>
 						Save
 					</Button>
 				</section>
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import Appointment from 'components/Appointment/index';
-import Form from 'components/Appointment/Form';
 
 describe('Appointment', () => {
 	it('renders without crashing', () => {
